7b205070c23af457195dd695bb140585fbb56b3c
Dominick Leppich <dominick.leppich@gmail.com>
2018-04-20 00:27:29 +0200

    Rename BoardTest to BoardLogicTest and make BoardTest a test suite

---
+++ src/test/java/board/BoardLogicTest.java
@@ -0,0 +1,54 @@
+package board;
+
+import diavolopp.board.*;
+import diavolopp.preset.*;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class BoardLogicTest {
+    private static final int BOARD_SIZE = 10;
+    private Board board;
+    private Viewer viewer;
+
+    @Before
+    public void init() {
+        board = new BoardImpl(BOARD_SIZE);
+        viewer = board.viewer();
+    }
+
+    // ------------------------------------------------------------
+
+    @Test
+    public void createNewBoardWorks() {
+        board = new BoardImpl(BOARD_SIZE);
+        assertNotNull(board);
+    }
+
+    @Test
+    public void newBoardHasCorrectSize() {
+        assertEquals(BOARD_SIZE, viewer.getSize());
+    }
+
+    @Test
+    public void newBoardStartsWithCorrectPlayer() {
+        assertEquals(PlayerColor.White, viewer.getTurn());
+    }
+
+    @Test
+    public void newBoardHasCorrectStatus() {
+        assertEquals(Status.Ok, viewer.getStatus());
+    }
+
+    // ------------------------------------------------------------
+
+    @Test
+    public void changeTurnAfterValidLandMove() {
+        Move m = new Move(new Land(new Position(2, 2), new Position(3, 2),
+                new Position(2, 3)), new Land(new Position(4, 4), new
+                Position(5, 4), new Position(4, 5)));
+        board.make(m);
+        assertEquals(PlayerColor.Red, viewer.getTurn());
+    }
+
+}
--- src/test/java/board/BoardTest.java
+++ src/test/java/board/BoardTest.java
@@ -1,53 +1,12 @@
 package board;
 
-import diavolopp.board.*;
-import diavolopp.preset.*;
-import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
 
-import static org.junit.Assert.*;
-
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        BoardLogicTest.class,
+        BoardLandMoveTest.class
+})
 public class BoardTest {
-    private static final int BOARD_SIZE = 10;
-    private Board board;
-    private Viewer viewer;
-
-    @Before
-    public void init() {
-        board = new BoardImpl(BOARD_SIZE);
-        viewer = board.viewer();
-    }
-
-    // ------------------------------------------------------------
-
-    @Test
-    public void createNewBoardWorks() {
-        board = new BoardImpl(BOARD_SIZE);
-        assertNotNull(board);
-    }
-
-    @Test
-    public void newBoardHasCorrectSize() {
-        assertEquals(BOARD_SIZE, viewer.getSize());
-    }
-
-    @Test
-    public void newBoardStartsWithCorrectPlayer() {
-        assertEquals(PlayerColor.White, viewer.getTurn());
-    }
-
-    @Test
-    public void newBoardHasCorrectStatus() {
-        assertEquals(Status.Ok, viewer.getStatus());
-    }
-
-    // ------------------------------------------------------------
-
-    @Test
-    public void changeTurnAfterValidLandMove() {
-        Move m = new Move(new Land(new Position(2, 2), new Position(3, 2),
-                new Position(2, 3)), new Land(new Position(4, 4), new
-                Position(5, 4), new Position(4, 5)));
-        board.make(m);
-        assertEquals(PlayerColor.Red, viewer.getTurn());
-    }
 }
