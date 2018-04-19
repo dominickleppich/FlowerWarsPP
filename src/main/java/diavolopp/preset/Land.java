745a0ee2e66332fca13ba4c27e270845b34631ff
Dominick Leppich <dominick.leppich@gmail.com>
2018-04-19 18:46:19 +0200

    Add Land class and test cases

---
+++ src/main/java/diavolopp/preset/Land.java
@@ -0,0 +1,81 @@
+package diavolopp.preset;
+
+import java.util.*;
+
+public class Land {
+    private Position[] positions;
+
+    // ------------------------------------------------------------
+
+    public Land(final Position first, final Position second, final Position
+            third) {
+        positions = new Position[3];
+
+        setFirst(first);
+        setSecond(second);
+        setThird(third);
+
+        updateOrder();
+    }
+
+    private void updateOrder() {
+        Arrays.sort(positions);
+    }
+
+    // ------------------------------------------------------------
+
+    public Position getFirst() {
+        return positions[0];
+    }
+
+    private void setFirst(final Position first) {
+        if (first == null)
+            throw new IllegalArgumentException("first cannot be null");
+        positions[0] = first;
+    }
+
+    public Position getSecond() {
+        return positions[1];
+    }
+
+    private void setSecond(final Position second) {
+        if (second == null)
+            throw new IllegalArgumentException("second cannot be null");
+        positions[1] = second;
+    }
+
+    public Position getThird() {
+        return positions[2];
+    }
+
+    private void setThird(final Position third) {
+        if (third == null)
+            throw new IllegalArgumentException("third cannot be null");
+        positions[2] = third;
+    }
+
+    // ------------------------------------------------------------
+
+    @Override
+    public int hashCode() {
+        return (getFirst().hashCode() * Position.MAX_COLUMN * Position
+                .MAX_ROW + getSecond().hashCode()) * Position
+                .MAX_COLUMN * Position.MAX_ROW + getThird().hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null)
+            return false;
+        if (!(o instanceof Land))
+            return false;
+        Land l = (Land) o;
+        return Arrays.equals(positions, l.positions);
+    }
+
+    @Override
+    public String toString() {
+        return getFirst().toString() + "+" + getSecond().toString() + "+" +
+                getThird().toString();
+    }
+}
---
+++ src/test/java/diavolopp/preset/LandTest.java
@@ -0,0 +1,200 @@
+package diavolopp.preset;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class LandTest {
+    private Position first, second, third;
+
+    @Before
+    public void init() {
+        first = new Position(2, 3);
+        second = new Position(4, 5);
+        third = new Position(6, 7);
+    }
+
+    // ------------------------------------------------------------
+
+    @Test
+    public void creatingNewLandWithValidValuesWorks() {
+        Land l = new Land(first, second, third);
+        assertNotNull(l);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void creatingNewLandWithNullFirstThrowsException() {
+        new Land(null, second, third);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void creatingNewLandWithNullSecondThrowsException() {
+        new Land(first, null, third);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void creatingNewLandWithNullThirdThrowsException() {
+        new Land(first, second, null);
+    }
+
+    // ------------------------------------------------------------
+    // * Getters *
+
+    @Test
+    public void getFirstReturnsCorrectValue() {
+        Land l = new Land(first, second, third);
+        assertEquals(first, l.getFirst());
+    }
+
+    @Test
+    public void getSecondReturnsCorrectValue() {
+        Land l = new Land(first, second, third);
+        assertEquals(second, l.getSecond());
+    }
+
+    @Test
+    public void getThirdReturnsCorrectValue() {
+        Land l = new Land(first, second, third);
+        assertEquals(third, l.getThird());
+    }
+
+    // ------------------------------------------------------------
+    // * Equals *
+
+    @Test
+    public void equalsOnEqualLandsReturnsTrue() {
+        Land a = new Land(new Position(3, 4), new Position(5, 6), new
+                Position(7, 8));
+        Land b = new Land(new Position(3, 4), new Position(5, 6), new
+                Position(7, 8));
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void equalsOnPermutedLandsReturnsTrue1() {
+        Land a = new Land(first, second, third);
+        Land b = new Land(first, third, second);
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void equalsOnPermutedLandsReturnsTrue2() {
+        Land a = new Land(first, second, third);
+        Land b = new Land(second, first, third);
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void equalsOnPermutedLandsReturnsTrue3() {
+        Land a = new Land(first, second, third);
+        Land b = new Land(second, third, first);
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void equalsOnPermutedLandsReturnsTrue4() {
+        Land a = new Land(first, second, third);
+        Land b = new Land(third, first, second);
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void equalsOnPermutedLandsReturnsTrue5() {
+        Land a = new Land(first, second, third);
+        Land b = new Land(third, second, first);
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void equalsOnUnequalFirstPositionReturnsFalse() {
+        Land a = new Land(new Position(3, 4), new Position(5, 6), new
+                Position(7, 8));
+        Land b = new Land(new Position(3, 5), new Position(5, 6), new
+                Position(7, 8));
+        assertNotEquals(a, b);
+    }
+
+    @Test
+    public void equalsOnUnequalSecondPositionReturnsFalse() {
+        Land a = new Land(new Position(3, 4), new Position(5, 6), new
+                Position(7, 8));
+        Land b = new Land(new Position(3, 4), new Position(7, 6), new
+                Position(7, 8));
+        assertNotEquals(a, b);
+    }
+
+    @Test
+    public void equalsOnUnequalThirdPositionReturnsFalse() {
+        Land a = new Land(new Position(3, 4), new Position(5, 6), new
+                Position(7, 8));
+        Land b = new Land(new Position(3, 4), new Position(5, 6), new
+                Position(8, 8));
+        assertNotEquals(a, b);
+    }
+
+    @Test
+    public void equalsOnNullReturnsFalse() {
+        Land l = new Land(first, second, third);
+        assertNotEquals(l, null);
+    }
+
+    @Test
+    public void equalsOnWrongTypeReturnsFalse() {
+        Land l = new Land(first, second, third);
+        assertNotEquals(l, new Object());
+    }
+
+    // ------------------------------------------------------------
+    // * Other stuff *
+
+    @Test
+    public void equalLandsReturnSameHashCode() {
+        Land a = new Land(new Position(3, 4), new Position(5, 6), new
+                Position(7, 8));
+        Land b = new Land(new Position(3, 4), new Position(5, 6), new
+                Position(7, 8));
+        assertEquals(a.hashCode(), b.hashCode());
+    }
+
+    @Test
+    public void equalPermutedLandsReturnSameHashCode1() {
+        Land a = new Land(first, second, third);
+        Land b = new Land(first, third, second);
+        assertEquals(a.hashCode(), b.hashCode());
+    }
+
+    @Test
+    public void equalPermutedLandsReturnSameHashCode2() {
+        Land a = new Land(first, second, third);
+        Land b = new Land(second, first, third);
+        assertEquals(a.hashCode(), b.hashCode());
+    }
+
+    @Test
+    public void equalPermutedLandsReturnSameHashCode3() {
+        Land a = new Land(first, second, third);
+        Land b = new Land(second, third, first);
+        assertEquals(a.hashCode(), b.hashCode());
+    }
+
+    @Test
+    public void equalPermutedLandsReturnSameHashCode4() {
+        Land a = new Land(first, second, third);
+        Land b = new Land(third, first, second);
+        assertEquals(a.hashCode(), b.hashCode());
+    }
+
+    @Test
+    public void equalPermutedLandsReturnSameHashCode5() {
+        Land a = new Land(first, second, third);
+        Land b = new Land(third, second, first);
+        assertEquals(a.hashCode(), b.hashCode());
+    }
+
+    @Test
+    public void toStringReturnsSomething() {
+        Land l = new Land(first, second, third);
+        assertNotNull(l.toString());
+        assertNotEquals(l.toString(), "");
+    }
+}
