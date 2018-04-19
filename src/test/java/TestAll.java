2647273542b8246f5d2d73c3cebadadcbaccb693
Dominick Leppich <dominick.leppich@gmail.com>
2018-04-19 23:06:40 +0200

    Add BoardTest to AllTestSuite

--- src/test/java/TestAll.java
+++ src/test/java/TestAll.java
@@ -1,10 +1,12 @@
+import board.*;
 import diavolopp.preset.*;
 import org.junit.runner.*;
 import org.junit.runners.*;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        PresetTestSuite.class
+        PresetTestSuite.class,
+        BoardTest.class
 })
 public class TestAll {
 }
