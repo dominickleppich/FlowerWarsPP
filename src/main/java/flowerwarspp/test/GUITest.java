9d0e1e019845de58eba723e372030f5b97ec5649
Dominick Leppich <dominick.leppich@gmail.com>
2018-05-11 14:34:43 +0200

    Dummy test classes can create replay code

--- src/main/java/flowerwarspp/test/GUITest.java
+++ src/main/java/flowerwarspp/test/GUITest.java
@@ -12,62 +12,17 @@ public class GUITest {
         Viewer viewer = board.viewer();
         UIWindow window = new UIWindow();
         window.setViewer(viewer);
-
-        Flower w1, w2, w3, w4;
-        w1 = new Flower(new Position(5, 5), new Position(6, 5), new Position(5,
-                6));
-        w2 = new Flower(new Position(4, 4), new Position(5, 4), new Position(4,
-                5));
-        w3 = new Flower(new Position(7, 4), new Position(8, 4), new Position(7,
-                5));
-        w4 = new Flower(new Position(4, 7), new Position(5, 7), new Position(4,
-                8));
-
-        // Green lands
-        Flower r1, r2, r3, r4, r5, r6, r7, r8, r9, r10;
-        r1 = new Flower(new Position(2, 2), new Position(3, 2), new Position(2,
-                3));
-        r2 = new Flower(new Position(3, 2), new Position(4, 2), new Position(3,
-                3));
-        r3 = new Flower(new Position(3, 2), new Position(2, 3), new Position(3,
-                3));
-        r4 = new Flower(new Position(2, 3), new Position(3, 3), new Position(2,
-                4));
-        r5 = new Flower(new Position(1, 5), new Position(2, 5), new Position(2,
-                4));
-        r6 = new Flower(new Position(2, 4), new Position(3, 4), new Position(2,
-                5));
-        r7 = new Flower(new Position(2, 4), new Position(3, 4), new Position(3,
-                3));
-        r8 = new Flower(new Position(3, 3), new Position(4, 3), new Position(3,
-                4));
-        r9 = new Flower(new Position(3, 3), new Position(4, 3), new Position(4,
-                2));
-        r10 = new Flower(new Position(4, 2), new Position(5, 2), new Position
-                (4, 3));
+        int timeout = 500;
 
         List<Move> moves = new LinkedList<>();
 
-        // Set up test scenario
-        moves.add(new Move(w1, w2));
-        moves.add(new Move(r1, r2));
-        moves.add(new Move(w3, w4));
-        moves.add(new Move(r3, r4));
-        moves.add(new Move(new Ditch(new Position(5, 5), new Position(4, 5)
-        )));
-        moves.add(new Move(r5, r6));
-        moves.add(new Move(new Ditch(new Position(6, 5), new Position(7, 5)
-        )));
-        moves.add(new Move(r7, r8));
-        moves.add(new Move(new Ditch(new Position(5, 6), new Position(4, 7)
-        )));
-        moves.add(new Move(r9, r10));
 
-        Thread.sleep(1000);
+
+        Thread.sleep(timeout);
         for (Move m : moves) {
             board.make(m);
             window.update(m);
-            Thread.sleep(1000);
+            Thread.sleep(timeout);
         }
 
     }
--- src/main/java/flowerwarspp/test/MiniGameTest.java
+++ src/main/java/flowerwarspp/test/MiniGameTest.java
@@ -4,6 +4,8 @@ import flowerwarspp.board.*;
 import flowerwarspp.preset.*;
 import flowerwarspp.ui.graphical.*;
 
+import java.util.*;
+
 public class MiniGameTest {
     public static void main(String[] args) throws InterruptedException {
         Board board = new BoardImpl(10);
@@ -11,14 +13,67 @@ public class MiniGameTest {
         UIWindow window = new UIWindow();
         window.setViewer(viewer);
 
+        List<Move> moves = new LinkedList<Move>();
+
         while (viewer.getStatus() == Status.Ok) {
             Move move = window.request();
             board.make(move);
             window.update(move);
 
+            if (viewer.getStatus() != Status.Illegal)
+                moves.add(move);
 //            Thread.sleep(3000);
         }
 
+        // Print code to replay game
+        printReplayCode(moves);
+
         window.dispose();
+    }
+
+    private static void printReplayCode(List<Move> moves) {
+        for (Move m : moves) {
+            String newMoveString = "";
+            switch (m.getType()) {
+                case Flower:
+                    newMoveString = newFlowerMoveString(m);
+                    break;
+                case Ditch:
+                    newMoveString = newDitchMoveString(m);
+                    break;
+                case Surrender:
+                    newMoveString = "new Move(MoveType.Surrender)";
+                    break;
+                case End:
+                    newMoveString = "new Move(MoveType.End)";
+                    break;
+            }
+            System.out.println("moves.add(" + newMoveString + ");");
+        }
+    }
+
+    private static String newFlowerMoveString(Move move) {
+        return "new Move(" + newFlowerString(move.getFirstFlower()) + ", " +
+                newFlowerString(move.getSecondFlower()) + ")";
+    }
+
+    private static String newDitchMoveString(Move move) {
+        return "new Move(" + newDitchString(move.getDitch()) + ")";
+    }
+
+    private static String newFlowerString(Flower flower) {
+        return "new Flower(" + newPositionString(flower.getFirst()) + ", " +
+                newPositionString(flower.getSecond()) + ", " +
+                newPositionString(flower.getThird()) + ")";
+    }
+
+    private static String newDitchString(Ditch ditch) {
+        return "new Ditch(" + newPositionString(ditch.getFirst()) + ", " +
+                newPositionString(ditch.getSecond()) + ")";
+    }
+
+    private static String newPositionString(Position position) {
+        return "new Position(" + position.getColumn() + ", " + position
+                .getRow() + ")";
     }
 }
