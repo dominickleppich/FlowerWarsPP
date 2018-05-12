82863846a011bbb84475f656c32d74779e6ecba0
Dominick Leppich <dominick.leppich@gmail.com>
2018-05-12 17:11:57 +0200

    Add argument parser

---
+++ src/main/java/flowerwarspp/preset/PlayerType.java
@@ -0,0 +1,5 @@
+package flowerwarspp.preset;
+
+public enum PlayerType {
+    HUMAN, RANDOM_AI, SIMPLE_AI, ADVANCED_AI_1, ADVANCED_AI_2, ADVANCED_AI_3, ADVANCED_AI_4, ADVANCED_AI_5, REMOTE
+}
---
+++ src/main/java/flowerwarspp/test/CodeGenerationUtil.java
@@ -0,0 +1,50 @@
+package flowerwarspp.test;
+
+import flowerwarspp.preset.*;
+
+import java.util.*;
+
+public class CodeGenerationUtil {
+    public static void printReplayCode(List<Move> moves) {
+        for (Move m : moves) {
+            String newMoveString = "";
+            switch (m.getType()) {
+                case Flower:
+                    newMoveString = newFlowerMoveString(m);
+                    break;
+                case Ditch:
+                    newMoveString = newDitchMoveString(m);
+                    break;
+                case Surrender:
+                    newMoveString = "new Move(MoveType.Surrender)";
+                    break;
+                case End:
+                    newMoveString = "new Move(MoveType.End)";
+                    break;
+            }
+            System.out.println("moves.add(" + newMoveString + ");");
+        }
+    }
+
+    public static String newFlowerMoveString(Move move) {
+        return "new Move(" + newFlowerString(move.getFirstFlower()) + ", " + newFlowerString(
+                move.getSecondFlower()) + ")";
+    }
+
+    public static String newDitchMoveString(Move move) {
+        return "new Move(" + newDitchString(move.getDitch()) + ")";
+    }
+
+    public static String newFlowerString(Flower flower) {
+        return "new Flower(" + newPositionString(flower.getFirst()) + ", " + newPositionString(
+                flower.getSecond()) + ", " + newPositionString(flower.getThird()) + ")";
+    }
+
+    public static String newDitchString(Ditch ditch) {
+        return "new Ditch(" + newPositionString(ditch.getFirst()) + ", " + newPositionString(ditch.getSecond()) + ")";
+    }
+
+    public static String newPositionString(Position position) {
+        return "new Position(" + position.getColumn() + ", " + position.getRow() + ")";
+    }
+}
