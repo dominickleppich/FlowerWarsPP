838cd7605f419cb6d22abe46d0abfed933250bc2
Dominick Leppich <dominick.leppich@gmail.com>
2018-05-19 23:45:55 +0200

    Add match class

---
+++ src/main/java/flowerwarspp/game/Match.java
@@ -0,0 +1,80 @@
+package flowerwarspp.game;
+
+import flowerwarspp.board.*;
+import flowerwarspp.preset.*;
+import flowerwarspp.ui.*;
+import org.slf4j.*;
+
+public class Match {
+    private static final Logger logger = LoggerFactory.getLogger(Match.class);
+
+    // ------------------------------------------------------------
+
+    private Board board;
+    private Viewer viewer;
+    private Player[] players;
+    private Display display;
+    private Status status;
+
+    private int currentPlayer;
+
+    // ------------------------------------------------------------
+
+    public Match(int boardSize, Player redPlayer, Player bluePlayer, Display display) throws Exception {
+        board = new BoardImpl(boardSize);
+        viewer = board.viewer();
+        this.display = display;
+        display.setViewer(viewer);
+
+        currentPlayer = 0;
+
+        initPlayers(boardSize, redPlayer, bluePlayer);
+    }
+
+    private void initPlayers(int boardSize, Player redPlayer, Player bluePlayer) throws Exception {
+        // Init both players accordingly
+        redPlayer.init(boardSize, PlayerColor.Red);
+        bluePlayer.init(boardSize, PlayerColor.Blue);
+
+        // Save players into array
+        players = new Player[]{redPlayer, bluePlayer};
+    }
+
+    // ------------------------------------------------------------
+
+    public Status play() {
+        // Initial display reset
+        display.reset();
+
+        try {
+            // Main game loop
+            while (viewer.getStatus() == Status.Ok) {
+                // Request move
+                Move move = players[currentPlayer].request();
+
+                // Do move on match board
+                board.make(move);
+                status = viewer.getStatus();
+
+                // Confirm move to player and update other player
+                players[currentPlayer].confirm(status);
+                players[(currentPlayer + 1) % 2].update(move, status);
+
+                // Update display
+                display.update(move);
+
+                // Change to next player
+                currentPlayer = (currentPlayer + 1) % 2;
+            }
+
+            display.showStatus(status);
+
+            logger.debug("Match ended successfully with status: " + status);
+        } catch (Exception e) {
+            logger.error("Match ended with exception: " + e.getMessage());
+            return null;
+        }
+
+        return status;
+    }
+}
