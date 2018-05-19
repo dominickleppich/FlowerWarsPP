615cdf577372cc30f2ef8111ba833f5e9ecc1097
Dominick Leppich <dominick.leppich@gmail.com>
2018-05-19 19:34:49 +0200

    Fix ditch test (was broken)

--- src/test/java/board/BoardFlowerMoveDitchTest.java
+++ src/test/java/board/BoardFlowerMoveDitchTest.java
@@ -4,15 +4,27 @@ import flowerwarspp.board.*;
 import flowerwarspp.preset.*;
 import org.junit.*;
 
+import java.util.*;
+
 import static org.junit.Assert.*;
 
 public class BoardFlowerMoveDitchTest {
-    private static final int BOARD_SIZE = 3;
+    private static final int BOARD_SIZE = 10;
 
-    private static final Flower FIRST_BLOCKED_FLOWER = new Flower(new Position(2, 1), new Position(1,
-            2), new Position(2, 2));
-    private static final Flower SECOND_BLOCKED_FLOWER = new Flower(new Position(2, 1), new Position(3,
-            1), new Position(2, 2));
+    private static final Flower FIRST_BLOCKED_FLOWER = new Flower(new Position(3, 2), new Position(2, 3),
+            new Position(3, 3));
+    private static final Flower SECOND_BLOCKED_FLOWER = new Flower(new Position(3, 2), new Position(4, 2),
+            new Position(3, 3));
+
+    private static final Flower FIRST_VALID_RED_FLOWER = new Flower(new Position(2, 8), new Position(3, 8),
+            new Position(2, 9));
+    private static final Flower SECOND_VALID_RED_FLOWER = new Flower(new Position(3, 5), new Position(2, 6),
+            new Position(3, 6));
+
+    private static final Flower FIRST_VALID_BLUE_FLOWER = new Flower(new Position(5, 5), new Position(6, 5),
+            new Position(5, 6));
+    private static final Flower SECOND_VALID_BLUE_FLOWER = new Flower(new Position(4, 6), new Position(3, 7),
+            new Position(4, 7));
 
     private Board board;
     private Viewer viewer;
@@ -22,141 +34,111 @@ public class BoardFlowerMoveDitchTest {
         board = new BoardImpl(BOARD_SIZE);
         viewer = board.viewer();
 
-        board.make(new Move(new Flower(new Position(1, 1), new Position(2, 1), new
-                Position(1, 2)), new Flower(new Position(2, 2), new Position(3,
-                2), new Position(2, 3))));
-        board.make(new Move(new Flower(new Position(1, 3), new Position(2, 3), new
-                Position(1, 4)), new Flower(new Position(2, 2), new Position(1,
-                3), new Position(2, 3))));
-        board.make(new Move(new Ditch(new Position(2, 1), new Position(2, 2)
-        )));
+        // Set up board
+
+        List<Move> replayMoves = new LinkedList<>();
+
+        replayMoves.add(new Move(new Flower(new Position(2, 2), new Position(3, 2), new Position(2, 3)),
+                new Flower(new Position(3, 3), new Position(2, 4), new Position(3, 4))));
+        replayMoves.add(new Move(new Flower(new Position(8, 2), new Position(9, 2), new Position(8, 3)), new Flower(new Position(7, 3), new Position(6, 4), new Position(7, 4))));
+        replayMoves.add(new Move(new Ditch(new Position(3, 2), new Position(3, 3))));
+
+        for (Move m : replayMoves)
+            board.make(m);
     }
 
     // ------------------------------------------------------------
 
     @Test
     public void validFlowerMoveForRedPlayer() {
-        board.make(new Move(new Flower(new Position(1, 2), new Position(2, 2),
-                new Position(1, 3)), new Flower(new Position(3, 1), new
-                Position(2, 2), new Position(3, 2))));
+        // Do blue move before
+        board.make(new Move(FIRST_VALID_BLUE_FLOWER, SECOND_VALID_BLUE_FLOWER));
+
+        board.make(new Move(FIRST_VALID_RED_FLOWER, SECOND_VALID_RED_FLOWER));
 
         assertEquals(Status.Ok, viewer.getStatus());
     }
 
     @Test
     public void invalidFlowerMoveOnDitchBlockedFieldByRedPlayerOnFirstBlockedFieldByFirstFlower() {
-        Flower validFlower = new Flower(new Position(1, 2), new Position(2, 2),
-                new Position(1, 3));
+        // Do blue move before
+        board.make(new Move(FIRST_VALID_BLUE_FLOWER, SECOND_VALID_BLUE_FLOWER));
 
-        board.make(new Move(FIRST_BLOCKED_FLOWER, validFlower));
+        board.make(new Move(FIRST_BLOCKED_FLOWER, SECOND_VALID_RED_FLOWER));
 
         assertEquals(Status.Illegal, viewer.getStatus());
     }
 
     @Test
     public void invalidFlowerMoveOnDitchBlockedFieldByRedPlayerOnSecondBlockedFieldByFirstFlower() {
-        Flower validFlower = new Flower(new Position(1, 2), new Position(2, 2),
-                new Position(1, 3));
+        // Do blue move before
+        board.make(new Move(FIRST_VALID_BLUE_FLOWER, SECOND_VALID_BLUE_FLOWER));
 
-        board.make(new Move(SECOND_BLOCKED_FLOWER, validFlower));
+        board.make(new Move(SECOND_BLOCKED_FLOWER, SECOND_VALID_RED_FLOWER));
 
         assertEquals(Status.Illegal, viewer.getStatus());
     }
 
     @Test
     public void invalidFlowerMoveOnDitchBlockedFieldByRedPlayerOnFirstBlockedFieldBySecondFlower() {
-        Flower validFlower = new Flower(new Position(1, 2), new Position(2, 2),
-                new Position(1, 3));
+        // Do blue move before
+        board.make(new Move(FIRST_VALID_BLUE_FLOWER, SECOND_VALID_BLUE_FLOWER));
 
-        board.make(new Move(validFlower, FIRST_BLOCKED_FLOWER));
+        board.make(new Move(FIRST_VALID_RED_FLOWER, FIRST_BLOCKED_FLOWER));
 
         assertEquals(Status.Illegal, viewer.getStatus());
     }
 
     @Test
     public void invalidFlowerMoveOnDitchBlockedFieldByRedPlayerOnSecondBlockedFieldBySecondFlower() {
-        Flower validFlower = new Flower(new Position(1, 2), new Position(2, 2),
-                new Position(1, 3));
+        // Do blue move before
+        board.make(new Move(FIRST_VALID_BLUE_FLOWER, SECOND_VALID_BLUE_FLOWER));
 
-        board.make(new Move(validFlower, SECOND_BLOCKED_FLOWER));
+        board.make(new Move(FIRST_VALID_RED_FLOWER, SECOND_BLOCKED_FLOWER));
 
         assertEquals(Status.Illegal, viewer.getStatus());
     }
 
     @Test
     public void invalidFlowerMoveOnDitchBlockedFieldsByRedPlayerOnBothBlockedFields() {
+        // Do blue move before
+        board.make(new Move(FIRST_VALID_BLUE_FLOWER, SECOND_VALID_BLUE_FLOWER));
+
         board.make(new Move(FIRST_BLOCKED_FLOWER, SECOND_BLOCKED_FLOWER));
 
         assertEquals(Status.Illegal, viewer.getStatus());
     }
 
     @Test
-    public void invalidFlowerMoveOnDitchBlockedFieldByWhitePlayerOnFirstBlockedFieldByFirstFlower() {
-        // Do red move before
-        board.make(new Move(new Flower(new Position(1, 2), new Position(2, 2),
-                new Position(1, 3)), new Flower(new Position(3, 1), new
-                Position(2, 2), new Position(3, 2))));
-
-        Flower validFlower = new Flower(new Position(3, 1), new Position(4, 1), new
-                Position(3, 2));
-
-        board.make(new Move(FIRST_BLOCKED_FLOWER, validFlower));
+    public void invalidFlowerMoveOnDitchBlockedFieldByBluePlayerOnFirstBlockedFieldByFirstFlower() {
+        board.make(new Move(FIRST_BLOCKED_FLOWER, SECOND_VALID_BLUE_FLOWER));
 
         assertEquals(Status.Illegal, viewer.getStatus());
     }
 
     @Test
-    public void invalidFlowerMoveOnDitchBlockedFieldByWhitePlayerOnSecondBlockedFieldByFirstFlower() {
-        // Do red move before
-        board.make(new Move(new Flower(new Position(1, 2), new Position(2, 2),
-                new Position(1, 3)), new Flower(new Position(3, 1), new
-                Position(2, 2), new Position(3, 2))));
-
-        Flower validFlower = new Flower(new Position(3, 1), new Position(4, 1), new
-                Position(3, 2));
-
-        board.make(new Move(SECOND_BLOCKED_FLOWER, validFlower));
+    public void invalidFlowerMoveOnDitchBlockedFieldByBluePlayerOnSecondBlockedFieldByFirstFlower() {
+        board.make(new Move(SECOND_BLOCKED_FLOWER, SECOND_VALID_BLUE_FLOWER));
 
         assertEquals(Status.Illegal, viewer.getStatus());
     }
 
     @Test
-    public void invalidFlowerMoveOnDitchBlockedFieldByWhitePlayerOnFirstBlockedFieldBySecondFlower() {
-        // Do red move before
-        board.make(new Move(new Flower(new Position(1, 2), new Position(2, 2),
-                new Position(1, 3)), new Flower(new Position(3, 1), new
-                Position(2, 2), new Position(3, 2))));
-
-        Flower validFlower = new Flower(new Position(3, 1), new Position(4, 1), new
-                Position(3, 2));
-
-        board.make(new Move(validFlower, FIRST_BLOCKED_FLOWER));
+    public void invalidFlowerMoveOnDitchBlockedFieldByBluePlayerOnFirstBlockedFieldBySecondFlower() {
+        board.make(new Move(FIRST_VALID_BLUE_FLOWER, FIRST_BLOCKED_FLOWER));
 
         assertEquals(Status.Illegal, viewer.getStatus());
     }
 
     @Test
-    public void invalidFlowerMoveOnDitchBlockedFieldByWhitePlayerOnSecondBlockedFieldBySecondFlower() {
-        // Do red move before
-        board.make(new Move(new Flower(new Position(1, 2), new Position(2, 2),
-                new Position(1, 3)), new Flower(new Position(3, 1), new
-                Position(2, 2), new Position(3, 2))));
-
-        Flower validFlower = new Flower(new Position(3, 1), new Position(4, 1), new
-                Position(3, 2));
-
-        board.make(new Move(validFlower, SECOND_BLOCKED_FLOWER));
+    public void invalidFlowerMoveOnDitchBlockedFieldByBluePlayerOnSecondBlockedFieldBySecondFlower() {
+        board.make(new Move(FIRST_VALID_BLUE_FLOWER, SECOND_BLOCKED_FLOWER));
 
         assertEquals(Status.Illegal, viewer.getStatus());
     }
 
     @Test
-    public void invalidFlowerMoveOnDitchBlockedFieldsByWhitePlayerOnBothBlockedFields() {
-        // Do red move before
-        board.make(new Move(new Flower(new Position(1, 2), new Position(2, 2),
-                new Position(1, 3)), new Flower(new Position(3, 1), new
-                Position(2, 2), new Position(3, 2))));
-
+    public void invalidFlowerMoveOnDitchBlockedFieldsByBluePlayerOnBothBlockedFields() {
         board.make(new Move(FIRST_BLOCKED_FLOWER, SECOND_BLOCKED_FLOWER));
 
         assertEquals(Status.Illegal, viewer.getStatus());
