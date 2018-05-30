2bf4f291ca7fe36214c0fe1dfe0e9df80eb56104
Dominick Leppich <dominick.leppich@gmail.com>
2018-05-30 17:34:42 +0200

    Add redundant RemoteException to player methods

--- src/main/java/flowerwarspp/preset/Player.java
+++ src/main/java/flowerwarspp/preset/Player.java
@@ -3,8 +3,8 @@ package flowerwarspp.preset;
 import java.rmi.*;
 
 public interface Player extends Remote {
-    Move request() throws Exception;
-    void confirm(Status status) throws Exception;
-    void update(Move opponentMove, Status status) throws Exception;
-    void init(int boardSize, PlayerColor color) throws Exception;
+    Move request() throws Exception, RemoteException;
+    void confirm(Status status) throws Exception, RemoteException;
+    void update(Move opponentMove, Status status) throws Exception, RemoteException;
+    void init(int boardSize, PlayerColor color) throws Exception, RemoteException;
 }
