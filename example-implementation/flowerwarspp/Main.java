6e8fd02072d35662b6e63972395b493da26eea51
Dominick Leppich <dominick.leppich@gmail.com>
2018-06-03 22:31:07 +0200

    Add some more usages

--- example-implementation/flowerwarspp/Main.java
+++ example-implementation/flowerwarspp/Main.java
@@ -6,7 +6,13 @@ import flowerwarspp.boarddisplay.*;
 public class Main {
 	public static void main(String[] args) {
 		Board board = new MyBoard(6);
+		Viewer viewer = board.viewer();
 		BoardDisplay boardDisplay = new BoardDisplay();
-		boardDisplay.setViewer(board.viewer());
+		boardDisplay.setViewer(viewer);
+
+		Move move = new Move(MoveType.Surrender);
+		board.make(move);
+		boardDisplay.update(move);
+		boardDisplay.showStatus(viewer.getStatus());
 	}
 }
